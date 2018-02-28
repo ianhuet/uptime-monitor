4c59ac1c8cf98959b12510dcf059b4c558dcb03c
Ian Huet <ian@huet.info>
2018-02-28 13:51:09 +0000

    Setup .ENV configuration

--- index.php
+++ index.php
@@ -4,29 +4,16 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
+// Load .ENV configuration
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
-$app->get('/', function (Request $request, Response $response) {
-  $url = "http://ianhuet.quickconnect.to";
+// Create app instance
+$app = new \Slim\App();
 
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_NOBODY, true);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_exec($ch);
-  $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  curl_close($ch);
-
-  if (200==$retcode) {
-    $response->getBody()->write("Online");
-  } else {
-    $response->getBody()->write("Offline");
-  }
-
-  return $response;
-});
 
 $app->get('/check', function (Request $request, Response $response) {
-  $url = "http://ianhuet.quickconnect.to";
+  $url = getenv('UPTIME_ENDPOINT');
 
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_NOBODY, true);
@@ -44,10 +31,15 @@ $app->get('/check', function (Request $request, Response $response) {
   return $response;
 });
 
-// $app->get('/data/{name}', function (Request $request, Response $response, array $args) {
-//   $name = $args['name'];
-//   $response->getBody()->write("Hello, $name");
+$app->get('/', function (Request $request, Response $response) {
+  // return log from the last 1 day as JSON
+});
 
+// $app->get('/data/{days}', function (Request $request, Response $response, array $args) {
+  // return log from the last {days} day as JSON
+  // $days = $args['days'];
+
+//   $response->getBody()->write("Hello, $name");
 //   return $response;
 // });
 
